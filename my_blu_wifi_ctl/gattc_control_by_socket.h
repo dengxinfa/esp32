32fdc564da30868e3f57b27c59870a4e3a3e31d9
deng <1712866140@qq.com>
2018-01-22 23:22:18 -0800

    use a socket to control a BLE light bulb

---
+++ my_blu_wifi_ctl/gattc_control_by_socket.h
@@ -0,0 +1,71 @@
+#ifndef  __GATTC_CONTROL_BY_SOCKET_H_
+#define  __GATTC_CONTROL_BY_SOCKET_H_
+#include <stdint.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include "nvs.h"
+#include "nvs_flash.h"
+#include "controller.h"
+
+#include "bt.h"
+#include "esp_gap_ble_api.h"
+#include "esp_gattc_api.h"
+#include "esp_gatt_defs.h"
+#include "esp_bt_main.h"
+#include "esp_gatt_common_api.h"
+
+#define GATTC_TAG "GATTC_DEMO"
+#define REMOTE_SERVICE_UUID        0xfff0
+#define REMOTE_NOTIFY_CHAR_UUID    0xfff0
+#define PROFILE_NUM      1
+#define PROFILE_A_APP_ID 0
+#define INVALID_HANDLE   0
+
+static const char remote_device_name[] = "AmoRgbLight";
+static bool connect    = false;
+static bool get_server = false;
+static esp_gattc_char_elem_t *char_elem_result   = NULL;
+static esp_gattc_descr_elem_t *descr_elem_result = NULL;
+
+extern uint8_t turnon_light[4];
+extern uint8_t turnoff_light[4];
+
+
+
+
+/* eclare static functions */
+static void esp_gap_cb(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t *param);
+static void esp_gattc_cb(esp_gattc_cb_event_t event, esp_gatt_if_t gattc_if, esp_ble_gattc_cb_param_t *param);
+static void gattc_profile_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t gattc_if, esp_ble_gattc_cb_param_t *param);
+
+struct gattc_profile_inst {
+    esp_gattc_cb_t gattc_cb;
+    uint16_t gattc_if;
+    uint16_t app_id;
+    uint16_t conn_id;
+    uint16_t service_start_handle;
+    uint16_t service_end_handle;
+    uint16_t char_handle;
+    esp_bd_addr_t remote_bda;
+};
+
+/* One gatt-based profile one app_id and one gattc_if, this array will store the gattc_if returned by ESP_GATTS_REG_EVT */
+extern struct gattc_profile_inst gl_profile_tab[PROFILE_NUM] ;
+
+
+static void gattc_profile_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t gattc_if, esp_ble_gattc_cb_param_t *param);
+
+static void esp_gap_cb(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t *param);
+
+static void esp_gattc_cb(esp_gattc_cb_event_t event, esp_gatt_if_t gattc_if, esp_ble_gattc_cb_param_t *param);
+
+void app_BLE_init();
+
+
+
+
+
+
+#endif
+
